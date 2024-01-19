0a342580cc0d8aaec1968f66afa9253af6c9c2ef
Alex <alex@alex-dev.me>
2024-01-19 01:06:47 -0600

    Refactor DashboardLayout props

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,9 +2,9 @@ import { Sidebar } from "../components";
 
 export default function DashboardLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex">
