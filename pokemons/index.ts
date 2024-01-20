259affc7affd55a7b34b44953302d6c9e722a208
Alex <alex@alex-dev.me>
2024-01-20 00:37:40 -0600

    Add Pokemon interface and related types

--- pokemons/index.ts
+++ pokemons/index.ts
@@ -1,3 +1,7 @@
+// Types
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { Pokemon } from "./interfaces/pokemon";
+
+// Components
 export { PokemonGrid } from "./components/PokemonGrid";
