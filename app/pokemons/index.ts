e9f4f25ee9bbabbe648924615d989fd57cfe9bc8
Alex <alex@alex-dev.me>
2024-01-19 21:42:56 -0600

    Add Pokemon-related files and interfaces

---
+++ app/pokemons/index.ts
@@ -0,0 +1,3 @@
+export type { PokemonsResponse } from "./interfaces/pokemons-response";
+export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export { PokemonGrid } from "./components/PokemonGrid";
