b1d578faab36afe2ad075e17d787469c480f9f7e
Alex <alex@alex-dev.me>
2024-01-21 02:05:14 -0600

    Update dashboard layout styling

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,9 @@ export default function DashboardLayout({
     <div className="bg-gray-800 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex">
         <Sidebar />
-        <div className="min-h-screen text-slate-700">{children}</div>
+        <div className="min-h-screen w-full text-white bg-gray-900 bg-clip-border shadow-lg">
+          {children}
+        </div>
       </div>
     </div>
   );
