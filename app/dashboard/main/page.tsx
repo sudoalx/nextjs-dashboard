c95e79287c16420f0d1c7072566caa6c83c69334
Alex <alex@alex-dev.me>
2024-01-22 00:26:29 -0600

    Refactor dashboard main page

--- app/dashboard/main/page.tsx
+++ app/dashboard/main/page.tsx
@@ -1,12 +1,24 @@
+import { SimpleWidget } from "@/components";
+
 export const metadata = {
-  title: "Dashboard Main Page",
-  description: "Dashboard Main Page",
+  title: "Dashboard",
+  description: "Dashboard",
 };
 
-export default function MainPage() {
+export default function DashboardPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
-      <h1>Dashboard Main Page</h1>
+    <div className="flex flex-col w-full h-full bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
+        Dashboard
+      </h1>
+      <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+        Information
+      </h2>
+      <div className="flex flex-wrap p-4 gap-1 justify-center">
+        <SimpleWidget />
+        <SimpleWidget />
+        <SimpleWidget />
+      </div>
     </div>
   );
 }
