19b9556b9a1ce495384d0a46be6683322ae37908
Alex <alex@alex-dev.me>
2024-01-24 23:58:39 -0600

    Remove unused code for fetching Pokemons

--- app/dashboard/favorites/page.tsx
+++ app/dashboard/favorites/page.tsx
@@ -5,29 +5,7 @@ export const metadata = {
   description: "List of Pokemons from the PokeAPI",
 };
 
-const getPokemons = async (
-  limit = 20,
-  offset = 0
-): Promise<SimplePokemon[]> => {
-  try {
-    const data: PokemonsResponse = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-    ).then((res) => res.json());
-
-    const pokemons = data.results.map((pokemon) => ({
-      id: parseInt(pokemon.url.split("/").at(-2) || "1"),
-      name: pokemon.name,
-    }));
-
-    return pokemons;
-  } catch (error) {
-    console.error("Error fetching pokemons:", error);
-    throw error;
-  }
-};
-
 export default async function PokemonsPage() {
-  const pokemons = await getPokemons(150);
   return (
     <div className="flex flex-col items-center w-full h-full bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 p-4">
       <div className="flex flex-col items-center justify-evenly w-full mb-4 md:flex-row md:items-center md:justify-around">
