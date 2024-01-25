2efa7e4e0dc2cde1669668060f1427797b256c15
Alex <alex@alex-dev.me>
2024-01-24 22:41:39 -0600

    Refactor FavoritePokemons component to use useState hook

--- pokemons/components/FavoritePokemons.tsx
+++ pokemons/components/FavoritePokemons.tsx
@@ -3,10 +3,14 @@ import { useAppSelector } from "@/store";
 import { SimplePokemon } from "../interfaces/simple-pokemon";
 
 import { PokemonGrid } from "..";
+import { useState } from "react";
 
 export const FavoritePokemons = () => {
   const favorites: SimplePokemon[] = useAppSelector((state) =>
     Object.values(state.favpokemons)
   );
-  return <PokemonGrid pokemons={favorites} />;
+
+  const [pokemons, setPokemons] = useState(favorites);
+
+  return <PokemonGrid pokemons={pokemons} />;
 };
