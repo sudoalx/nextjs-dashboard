b4f928cd393c0c9a2c3cf25dbd039a089ef853d7
Alex <alex@alex-dev.me>
2024-01-24 23:58:46 -0600

    Add FavoritesEmpty component to display a message when there are no favorite pokemons

--- pokemons/components/FavoritePokemons.tsx
+++ pokemons/components/FavoritePokemons.tsx
@@ -4,6 +4,8 @@ import { SimplePokemon } from "../interfaces/simple-pokemon";
 
 import { PokemonGrid } from "..";
 import { useState } from "react";
+import { IoHeartDislike } from "react-icons/io5";
+import Link from "next/link";
 
 export const FavoritePokemons = () => {
   const favorites: SimplePokemon[] = useAppSelector((state) =>
@@ -12,5 +14,27 @@ export const FavoritePokemons = () => {
 
   const [pokemons, setPokemons] = useState(favorites);
 
-  return <PokemonGrid pokemons={pokemons} />;
+  return (
+    <>
+      {favorites.length === 0 ? (
+        <FavoritesEmpty />
+      ) : (
+        <PokemonGrid pokemons={pokemons} />
+      )}
+    </>
+  );
+};
+
+export const FavoritesEmpty = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 p-4">
+      <IoHeartDislike className="text-9xl text-gray-400 dark:text-gray-600" />
+      <h2>
+        No favorite pokemons yet.{" "}
+        <Link href={"/dashboard/pokemons"} className="text-blue-500">
+          Add favorites from the list!
+        </Link>
+      </h2>
+    </div>
+  );
 };
